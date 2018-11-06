3e72106059556389aafcb877c0ce9d2367838296
stadmin <stadmin@sim-tek.com.tr>
2018-11-06 10:23:11 +0300

    initial

---
+++ stripe.py
@@ -0,0 +1,11 @@
+"""
+Given an array of integers. Find the first mission 'positive integer' in linear time and
+constant space. In short, find the lowest positive interger that is not in array.
+
+Array can contain negatives or duplicates.
+
+For ex: [3,4,-1,1] => 2 [1,2,0] => 3
+
+"""
+
+
