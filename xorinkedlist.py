a35af6112fb3e904d68502a31e143f8e80487b83
stadmin <stadmin@sim-tek.com.tr>
2018-11-06 10:17:31 +0300

    initial

---
+++ xorinkedlist.py
@@ -0,0 +1,9 @@
+"""
+XOR linked list is a more memory efficient doubly linked list.
+
+Instead of each node holding next and prev, it holds a field named both which is an XOR of the next node and the previous.
+
+Implement an XOR linked list; it has an add(element) which adds the element at the end and
+get(index) return the node at index.
+
+"""
